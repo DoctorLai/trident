18dfa92ec68c9aca3f2126c53114c3a45d034860
justyy <dr.zhihua.lai@gmail.com>
2022-03-18 15:10:18 +0000

    init commit of trident python

---
+++ trident-py/trident/__init__.py
@@ -0,0 +1,7 @@
+from trident.tron import Tron
+from trident.async_tron import AsyncTron
+from trident.contract import Contract, ShieldedTRC20
+from trident.async_contract import AsyncContract
+
+TRX = 1_000_000
+SUN = 1
